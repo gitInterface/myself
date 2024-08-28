c4e80b6fd7b3caa66fe2c9db52242c4b96599127
gitInterface <97436826+gitInterface@users.noreply.github.com>
2024-08-28 21:13:41 +0800

    fix: bugs

--- js/libs.js
+++ js/libs.js
@@ -1,21 +1,27 @@
-// 動態加載CSS
-var head = document.getElementsByTagName('head')[0];
+window.onload = function () {
+    // 動態加載CSS
+    var head = document.getElementsByTagName('head')[0];
 
-var bootstrapCss = document.createElement('link');
-bootstrapCss.rel = 'stylesheet';
-bootstrapCss.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css';
-head.appendChild(bootstrapCss);
+    var bootstrapCss = document.createElement('link');
+    bootstrapCss.rel = 'stylesheet';
+    bootstrapCss.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css';
+    head.appendChild(bootstrapCss);
 
-var fontAwesomeCss = document.createElement('link');
-fontAwesomeCss.rel = 'stylesheet';
-fontAwesomeCss.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
-head.appendChild(fontAwesomeCss);
+    var fontAwesomeCss = document.createElement('link');
+    fontAwesomeCss.rel = 'stylesheet';
+    fontAwesomeCss.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
+    head.appendChild(fontAwesomeCss);
 
-// 動態加載JS
-var bootstrapJs = document.createElement('script');
-bootstrapJs.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js';
-document.body.appendChild(bootstrapJs);
+    // 動態加載JS
+    var bootstrapJs = document.createElement('script');
+    bootstrapJs.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js';
+    document.body.appendChild(bootstrapJs);
 
-var jQueryJs = document.createElement('script');
-jQueryJs.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.body.appendChild(jQueryJs);
+    var jQueryJs = document.createElement('script');
+    jQueryJs.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+    document.body.appendChild(jQueryJs);
+
+    var popperJs = document.createElement('script');
+    popperJs.src = 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js';
+    document.body.appendChild(popperJs);
+}
